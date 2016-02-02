be0d51b0b6e97f754e6ab3a837b2461accb99176
Drizzle <drizzlezhang@gmail.com>
2016-02-02 12:07:25 +0800

    first commit

---
+++ app/src/main/java/com/drizzle/androidutils/Imageutils.java
@@ -0,0 +1,65 @@
+package com.drizzle.androidutils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * 图片操作类
+ */
+public class Imageutils {
+
+	private Imageutils() {
+		/* cannot be instantiated */
+		throw new UnsupportedOperationException("cannot be instantiated");
+	}
+
+	/**
+	 * 固定宽度压缩图片
+	 */
+	public static Bitmap resizeBitmap(Bitmap bitmap, int newWidth) {
+		int width = bitmap.getWidth();
+		int height = bitmap.getHeight();
+		float temp = ((float) height) / ((float) width);
+		int newHeight = (int) ((newWidth) * temp);
+		float scaleWidth = ((float) newWidth) / width;
+		float scaleHeight = ((float) newHeight) / height;
+		Matrix matrix = new Matrix();
+		// resize the bitmap
+		matrix.postScale(scaleWidth, scaleHeight);
+		Bitmap resizedBitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, true);
+		return resizedBitmap;
+	}
+
+	/**
+	 * 从存储路径获取bitmap
+	 */
+	public static Bitmap getBitmapFromFile(String filePath) {
+		Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+		return bitmap;
+	}
+
+	/**
+	 * 保存图片到具体位置
+	 */
+	public void saveBitmap(Bitmap bitmap, String floderName, String saveName) {
+		File f = new File(floderName, saveName + ".jpg");
+		if (f.exists()) {
+			f.delete();
+		}
+		try {
+			FileOutputStream out = new FileOutputStream(f);
+			bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ app/src/main/java/com/drizzle/androidutils/TUtils.java
@@ -0,0 +1,64 @@
+package com.drizzle.androidutils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Toast统一管理类
+ */
+public class TUtils {
+	private static Toast toast;
+
+	private TUtils() {
+		/* cannot be instantiated */
+		throw new UnsupportedOperationException("cannot be instantiated");
+	}
+
+	/**
+	 * 短时间显示Toast
+	 */
+	public static void showShort(Context context, CharSequence message) {
+		if (toast == null) {
+			toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+		} else {
+			toast.setText(message);
+		}
+		toast.show();
+	}
+
+	/**
+	 * 短时间显示Toast
+	 */
+	public static void showShort(Context context, int message) {
+		if (toast == null) {
+			toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+		} else {
+			toast.setText(message);
+		}
+		toast.show();
+	}
+
+	/**
+	 * 长时间显示Toast
+	 */
+	public static void showLong(Context context, CharSequence message) {
+		if (toast == null) {
+			toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+		} else {
+			toast.setText(message);
+		}
+		toast.show();
+	}
+
+	/**
+	 * 长时间显示Toast
+	 */
+	public static void showLong(Context context, int message) {
+		if (toast == null) {
+			toast = Toast.makeText(context, message, Toast.LENGTH_LONG);
+		} else {
+			toast.setText(message);
+		}
+		toast.show();
+	}
+}
